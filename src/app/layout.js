dc6a0461fd8a59f7ad38afc5fff0a943f4df212a
Komal <ks989156@gmail.com>
2025-05-20 00:20:55 +0530

    fix hydration err

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning={true}>
         {children}
       </body>
     </html>
