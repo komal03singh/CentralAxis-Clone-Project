b8bb2e584abda8e84c255ad0b7010bad6856bcb4
Komal <ks989156@gmail.com>
2025-05-19 21:20:12 +0530

    first commit

---
+++ src/Components/Card1.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Image from "next/image";
+
+
+function Card1({heading,content,ulList=[],videoUrl}) {
+
+  return (
+    <div className='flex min-h-96 w-full rounded-lg border border-white/10 bg-[#18181B] '>
+
+        <div className='flex flex-col gap-8 p-10 pt-10 md:flex-1 md:p-10 w-1/2'>
+             <div className='flex flex-col items-start gap-2'>
+              <h2 className='text-2xl font-medium md:text-3xl'>{heading}</h2>
+              <p className='font-normal md:text-lg text-[#A1A1AA]'>{content}</p>
+             </div>
+             <ul className='flex flex-col items-start gap-3 pl-2 md:text-lg'>
+              {ulList?.map((List,index)=>(
+                <li className='flex items-center gap-4 font-normal text-[#A1A1AA]' key={index}>
+
+                  <img className='w-6 h-6 bg-[#787882] rounded-full' src="tick.svg" alt="" />
+
+                  
+                  
+                  {List}</li>
+              ))}
+            </ul>
+        </div>
+           
+        <div className='w-1/2 flex justify-center items-center'>
+          <div className='p-2 md:h-auto md:w-[360px] lg:w-[480px] xl:w-[560px]'> 
+            <video autoPlay muted loop src={videoUrl}></video>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Card1
--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,154 @@
+"use client";
+
+import Card1 from "@/Components/Card1";
+import Card1Data from "@/Data/Card1Data";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+  return (
+    <>
+      <nav className="flex sticky border-white/10 top-0 bg-black/40 backdrop:blur-xl justify-center items-center border-b">
+        <div className="container  flex justify-between items-center h-16 py-4">
+          <div className="flex items-center w-fullgap-1 py-2 ">
+            <a href="/">
+              <img className="p-2 w-14 h-15" src="logo.webp" alt="logo" />
+            </a>
+            <a className="text-2xl font-semibold" href="/">CentralAxis</a>
+          </div>
+
+          <div className="flex font-medium text-sm space-x-6">
+            <div className="group relative py-4">
+              <button className=" hover:cursor-pointer transition-colors group-hover:text-foreground/80" >Data Center Infrastructure Management<span className="ml-1">▼</span></button>
+                  <div className="opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-500 ease-in-out flex flex-col absolute right-0 mt-2 shadow-lg p-4 rounded-md bg-black border border-white/10 w-69 gap-4 z-40 ring-1 ring-border ring-opacity-5">
+                      <a href="" className="hover:text-foreground/80">Monitoring Devices</a>
+                      <a href="" className="hover:text-foreground/80">Asset Tracking</a>
+                      <a href="" className="hover:text-foreground/80">Network Management</a>
+                      <a href="" className="hover:text-foreground/80">Change Management</a>
+                      <a href="" className="hover:text-foreground/80">Colocation Management</a>
+                      <a href="" className="hover:text-foreground/80">Data Center Planner</a>
+                      <a href="" className="hover:text-foreground/80">Firmware Management</a>
+                      <a href="" className="hover:text-foreground/80">IP Address Management</a>
+                      <a href="" className="hover:text-foreground/80">Customer Portal</a>
+                      <a href="" className="hover:text-foreground/80">Custom Insights</a>
+                      <a href="" className="hover:text-foreground/80">Custom Data Center Solutions</a>
+                    </div>
+            </div>
+            <div className="group relative py-4">
+              <button className=" hover:cursor-pointer transition-colors group-hover:text-foreground/80" >Intelligence Solutions<span className="ml-1">▼</span></button>
+                  <div className="opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-500 ease-in-out flex flex-col absolute right-0 mt-2 shadow-lg p-4 rounded-md bg-black border border-white/10 w-69 gap-4 z-40 ring-1 ring-border ring-opacity-5">
+                      <a href="" className="hover:text-foreground/80">Digital Twins</a>
+                      <a href="" className="hover:text-foreground/80">Smart Power Mapping</a>
+                      <a href="" className="hover:text-foreground/80">Predictive Failure and Alerting</a>
+                      <a href="" className="hover:text-foreground/80">Smart Control Systems</a>
+                    </div>
+            </div>
+            <div className="group relative py-4">
+              <button className=" hover:cursor-pointer transition-colors group-hover:text-foreground/80" >Data Center Compliance<span className="ml-1">▼</span></button>
+                  <div className="opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-500 ease-in-out flex flex-col absolute right-0 mt-2 shadow-lg p-4 rounded-md bg-black border border-white/10 w-69 gap-4 z-40 ring-1 ring-border ring-opacity-5">
+                      <a href="" className="hover:text-foreground/80">DCOI Compliance</a>
+                      <a href="" className="hover:text-foreground/80">(EU) 2023/1791</a>
+                      <a href="" className="hover:text-foreground/80">EN 50600</a>
+                      <a href="" className="hover:text-foreground/80">LEED Certification</a>
+                      <a href="" className="hover:text-foreground/80">ISO/IEC 30134-2</a>
+                      <a href="" className="hover:text-foreground/80">(EPA) ENERGY STAR</a>
+                      <a href="" className="hover:text-foreground/80">European Commission Joint Research Centre (JRC)</a>
+                    </div>
+            </div>
+            <div className="group relative py-4">
+              <button className=" hover:cursor-pointer transition-colors group-hover:text-foreground/80" ><span className="ml-1">▼</span>Sustainability Blog</button>
+                  <div className="opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-500 ease-in-out flex flex-col absolute right-0 mt-2 shadow-lg p-4 rounded-md bg-black border border-white/10 w-69 gap-4 z-40 ring-1 ring-border ring-opacity-5">
+                      <a href="" className="hover:text-foreground/80">Energy Efficiency</a>
+                      <a href="" className="hover:text-foreground/80">Stranded Energy Map</a>
+                      <a href="" className="hover:text-foreground/80">Environmental Product Declaration</a>
+                      <a href="" className="hover:text-foreground/80">Renewables Integration</a>
+                      <a href="" className="hover:text-foreground/80">Power Resource Management</a>
+                  </div>
+            </div>
+          </div>
         </div>
+      </nav>
+
+      <main className="flex flex-col relative h-auto w-full -z-10 ">
+        <section className="relative w-full lg:h-auto grid grid-cols-[120px_auto_120px] grid-rows-[320px_100px] border-b border-white/10">
+          <div className="border-r border-white/10"></div>
+          <div className="flex flex-col items-center" >
+            <div className="border-red-100 mt-20 max-w-4xl">
+              <h1 className="text-6xl font-medium text-center">Data Center Management Software</h1>
+              <p className="text-center text-base mt-2 text-[#787882]">Increase uptime, reduce costs, automate operations, and stay compliant.</p>
+              <p className="text-center text-base mt-4 text-[#787882]">Built for colocations, data centers, and cloud provoders.</p>
+            </div>
+          </div>          
+          <div className="border-l-1 border-white/10"></div>          
+          <div className="border-t-1 border-r-1 border-white/10"></div>
+          <div className="flex justify-center border-t-1 border-white/10">
+            <a href="https://calendly.com/margarita-centralaxis/30min" className="flex justify-center items-center border-1 border-white/10 w-2/6 h-14 bg-[#7869dc] -z-10">
+              Request Demo
+            </a>
+          
+          </div>
+          <div className="border-t-1 border-l-1 border-white/10"></div>
+        </section>
+
+        <section className="py-14 flex flex-col items-center">
+          <h4 className="text-[#787882]">Seamless integrations with any data center hardware</h4>
+          <div className="mt-8 p-4 overflow-hidden">
+            <div className="flex gap-14 animate-[var(--animate-scroll)]">
+              <img className="w-32 h-20" src="/logos/nvidia.svg" alt="nvidia logo" />
+              <img className="w-32 h-20" src="/logos/amd.svg" alt="nvidia logo" />
+              <img className="w-32 h-20" src="/logos/cisco.svg" alt="nvidia logo" />
+              <img className="w-32 h-20" src="/logos/dell.svg" alt="nvidia logo" />
+              <img className="w-32 h-20" src="/logos/hp.svg" alt="nvidia logo" />
+              <img className="w-32 h-20" src="/logos/intel.svg" alt="nvidia logo" />
+              <img className="w-32 h-20" src="/logos/lenovo.svg" alt="nvidia logo" />
+              <img className="w-28 h-16" src="/logos/schneider.svg" alt="nvidia logo" />
+              <img className="w-32 h-20" src="/logos/supermicro.svg" alt="nvidia logo" />
+              <img className="w-32 h-20" src="/logos/nvidia.svg" alt="nvidia logo" />
+              <img className="w-32 h-20" src="/logos/amd.svg" alt="nvidia logo" />
+              <img className="w-32 h-20" src="/logos/cisco.svg" alt="nvidia logo" />
+              <img className="w-32 h-20" src="/logos/dell.svg" alt="nvidia logo" />
+              <img className="w-32 h-20" src="/logos/hp.svg" alt="nvidia logo" />
+              <img className="w-32 h-20" src="/logos/intel.svg" alt="nvidia logo" />
+              <img className="w-32 h-20" src="/logos/lenovo.svg" alt="nvidia logo" />
+              <img className="w-28 h-16" src="/logos/schneider.svg" alt="nvidia logo" />
+              <img className="w-32 h-20" src="/logos/supermicro.svg" alt="nvidia logo" />
+          </div>
+
+          </div>
+        </section>
+
+        <section className="flex flex-col gap-10 container mx-auto px-6 py-14">
+            <div  className="flex flex-col items-center gap-3 self-center ">
+              <h3 className="text-[#787882] bg-[#18181B] text-base font-medium px-3.5 pb-px min-h-7 rounded-full">Built for AI</h3>
+              <h2 className="text-3xl font-medium md:text-4xl">Modernizing the Digital Backbone</h2>
+              <p className=" text-[#787882] max-w-screen-md text-center text-lg font-light md:text-xl">Data center software (DCIM, BMS and EPMS) has stagnated. We've built a better way.</p>
+            </div>
+            <div className="flex flex-col gap-6">
+              {Card1Data?.map((data,index)=>(
+                <Card1 key={index} heading={data.heading} content={data.content} ulList={data.ulList} videoUrl={data.videoUrl}/>
+
+              ))}
+              
+            </div>
+        </section >
+
+        <section className='container mx-auto px-6 py-14'>
+          <div className="bg-[#151021] flex flex-col lg:flex-row rounded-xl gap-9 p-6 lg:justify-between lg:p-10">
+            <div className="flex flex-col gap-2">
+              <h1 className="text-3xl font-medium lg:text-4xl">We've Built the Future of Data Centers</h1>
+              <p className="text-[#787882] text-lg lg:text-xl">Contact our team for a demo</p>
+
+            </div>
+            <div className="flex flex-col gap-3 justify-center items-center">
+              <a className=" px-3.5 text-sm font-normal md:px-5 bg-[#8172EE] rounded-full p-1.5">Set Up a Time</a>
+              <a className="px-3.5 text-sm font-normal md:px-5 border-white/10 rounded-full p-1.5 bg-[#27272A]">Learn More</a>
+            </div>
+          </div>
+
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
